24bfbe9e683fb2b82e61f080603e912924992d72
steve liao <mynamelky@gmail.com>
2017-05-25 00:46:31 +0800

    Add lottery

--- app/js/lottery.js
+++ app/js/lottery.js
@@ -5,3 +5,93 @@ import Calculate from './lottery/calculate.js';
 import Interface from './lottery/interface.js';
 import $ from 'jquery';
 
+// 深度拷贝
+const copyProperties = function(target, source) {
+    for(let key of Reflect.ownKeys(source)) { // 拿到原对象所有属性
+        if(key!=='constructor' && key!=='prototype' && key!=='name') {
+            let desc = Object.getOwnPropertyDescriptor(source, key); // 返回source对象上自有属性(key)对应的属性描述
+            Object.defineProperty(target, key, desc); // 直接在target对象上定义一个新属性，进而达到深拷贝
+        }
+    }
+}
+
+// 多重继承
+const mix = function(...mixins) {
+    class Mix{};
+    for(let mixin of mixins) {
+        copyProperties(Mix, mixin);
+        copyProperties(Mix.prototype, mixin.prototype);
+    }
+    return Mix;
+}
+
+class Lottery extends mix(Base, Calculate, Interface, Timer){
+    constructor(name='syy', cname='11选5', issue='**', state='**') {
+
+        super();
+
+        this.name = name;
+        this.cname = cname;
+        this.issue = issue;
+        this.state = state;
+        
+        this.omit = new Map();
+        this.open_code = new Set();
+        this.open_code_list = new Set();
+        this.play_list = new Map();
+        this.number = new Set();
+        // Dom元素
+        this.el = '';
+        this.issue_el = '#curr_issue';
+        this.countdown_el = '#countdown';
+        this.state_el = '.state_el';
+        this.cart_el = '.codelist';
+        this.omit_el = '';
+        this.cur_play = 'r5';
+        // 初始化
+        this.initPlayList();
+        this.initNumber();
+        this.updateState();
+        this.initEvent();
+    }
+
+    updateState() {
+        let self = this;
+
+        this.getState().then(function(res){
+            self.issue = res.issue;
+            self.end_time = res.end_time;
+            self.state = res.state;
+
+            $(self.issue_el).text(res.issue);
+
+            self.countdown(res.end_time, function(time) {
+                $(self.countdown_el).html(time);
+            }, function() {
+                setTimeout(function() {
+                    self.updateState();
+                    self.getOmit(self.issue).then(function(res) {
+                        // 更新遗漏后...
+                    });
+                    self.getOpenCode(self.issue).then(function(res) {
+                        // 更新奖号后...
+                    });
+                }, 500);
+            });
+        });
+    }
+
+    /**
+     * [initEvent 初始化事件]
+     */
+    initEvent() {
+        let self = this;
+        $('#plays').on('click', 'li', self.changePlayNav.bind(self));
+        $('.boll-list').on('click', '.btn-boll', self.toggleCodeActive.bind(self));
+        $('#confirm_sel_code').on('click', self.addCode.bind(self));
+        $('.dxjo').on('click', 'li', self.assistHandle.bind(self));
+        $('.qkmethod').on('click', '.btn-middle', self.getRandomCode.bind(self));
+    }
+}
+
+export default Lottery;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,56 @@
 var express = require('express');
+var mockjs = require('mockjs');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// 输出当前的状态、期号、截止时间
+var makeIssue = function() {
+  var date = new Date();
+  var first_issue_date = new Date();
+  first_issue_date.setHours(9);
+  first_issue_date.setMinutes(10);
+  first_issue_date.setSeconds(0);
+  var end_issue_date = new Date(first_issue_date.getTime()+77*10*60*1000);
+  
+  var cur_issue, end_time, state;
+  if(date.getTime()-first_issue_date.getTime()>0 && date.getTime()-end_issue_date.getTime()<0) {
+    // 正常销售
+    var cur_issue_date = new Date();
+    cur_issue_date.setHours(9);
+    cur_issue_date.setMinutes(0);
+    cur_issue_date.setSeconds(0);
+    // 剩余时间
+    var minus_time = date.getTime() - cur_issue_date.getTime();
+    var h = Math.ceil(minus_time/1000/60/10);
+    var end_date = new Date(cur_issue_date.getTime()+1000*60*10*h);
+    end_time = end_date.getTime();
+    cur_issue = [end_date.getFullYear(), ('0'+end_date.getMonth()+1).slice(-2), ('0'+end_date.getDate()).slice(-2), ('0'+h).slice(-2)].join('');
+  }else{
+    // 今天销售已截止
+    if(date.getDate()!=end_issue_date.getDate()) {
+      first_issue_date.setDate(first_issue_date.getDate()+1);
+    }
+    end_time = first_issue_date.getTime();
+    cur_issue = [first_issue_date.getFullYear(), ('0'+first_issue_date.getMonth()+1).slice(-2), ('0'+first_issue_date.getDate()).slice(-2), '01'].join('');
+  }
+  // 10分钟一期：前八分钟销售，后两分钟是开奖中
+  var cur_date = new Date();
+  if(end_time-cur_date.getTime() > 1000*60*2) {
+    state = '正在销售';
+  }else{
+    state = '开奖中';
+  }
+
+  return {
+    issue: cur_issue,
+    state: state,
+    end_time: end_time
+  }
+  
+}
 
 module.exports = router;
